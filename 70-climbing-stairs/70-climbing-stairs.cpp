571a4e5f9263400f55a59a339e14f1dd51696fa7
nerja25390 <103855247+nerja25390@users.noreply.github.com>
2022-05-20 16:23:03 +0100

    Time: 3 ms (31.82%), Space: 5.9 MB (56.26%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        std::vector<int> v = {1, 2};
+        
+        for (int i = 2; i < n; i++){
+            v.push_back(v[i-1]+v[i-2]);
+        }
+        return v[n-1];
+    }
+};
