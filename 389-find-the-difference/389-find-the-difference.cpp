17ccf7135fa8f6debf9cd688dfb567ddb96ad4ae
nerja25390 <103855247+nerja25390@users.noreply.github.com>
2022-05-22 12:57:19 +0100

    Time: 0 ms (100.00%), Space: 6.7 MB (68.07%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans=0;
+        for(auto &x: t){
+            ans += x;}
+        
+        for(auto &x: s){
+            ans -= x;}
+        
+        return char(ans);
+    }   
+};
