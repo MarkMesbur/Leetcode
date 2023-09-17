063933a099a165653eaa7d6018895af5785152f9
Mark Mesbur <103855247+MarkMesbur@users.noreply.github.com>
2023-09-17 14:36:21 +0200

    Time: 3 ms (73.67%), Space: 9.6 MB (12.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         std::sort(strs.begin(), strs.end());
-        string lcp = "", first = strs[0], last = strs[strs.size() - 1];
+        string lcp = "";
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+
         for (int i = 0; i < min(first.length(), last.length()); i++){
             if (first[i] != last[i]){
                 return lcp;
